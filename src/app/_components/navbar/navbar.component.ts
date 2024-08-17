8f5de942c8998e6ff5e999e60ec29072a1d0e31d
José Lucas <gomessze03@gmail.com>
2024-08-17 15:01:18 -0300

    close #5: using NgOnInit and NgDestroy

--- src/app/_components/navbar/navbar.component.ts
+++ src/app/_components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-
+export class NavbarComponent implements OnInit{
+  ngOnInit(): void {
+    console.log('Navbar inicializou');
+  }
 }
