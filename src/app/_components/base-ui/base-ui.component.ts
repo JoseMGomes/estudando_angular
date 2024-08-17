f2558583b7aa0f2d1d05e3b990032e872b91ccc8
José Lucas <gomessze03@gmail.com>
2024-08-17 15:50:41 -0300

    close #7: using Ng-Content

---
+++ src/app/_components/base-ui/base-ui.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-ui',
+  templateUrl: './base-ui.component.html',
+  styleUrl: './base-ui.component.css'
+})
+export class BaseUiComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { FooterComponent } from './_components/footer/footer.component';
     HomeComponent,
     NavbarComponent,
     FooterComponent,
+    BaseUiComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
