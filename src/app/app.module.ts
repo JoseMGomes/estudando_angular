ce7ef51b1d9732de001f73f38f51c274fe64d46d
José Lucas <gomessze03@gmail.com>
2024-08-17 15:40:10 -0300

    close #6: using NgDoCheck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { ButtonComponent } from './_components/button/button.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { FooterComponent } from './_components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ButtonComponent
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements DoCheck{
 
+  num: number = 0;
+
+  adiciona1(){
+    this.num++;
+  }
+  remove1(){
+    this.num--;
+  }
+
+  ngDoCheck(): void {
+    console.log('Uma mudança foi realizada')
+  }
 }
